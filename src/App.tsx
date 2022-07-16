eb896686f38b82abf778e38da2d1331100b047ed
yuran1811 <trieuvanbd123@gmail.com>
2022-07-16 22:17:06 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import AudioSphere from "./components/AudioSphere";
+
+const App = () => (
+  <div className="bg-slate-900 w-screen h-screen overflow-x-hidden overflow-y-auto">
+    <AudioSphere />
+  </div>
+);
+
+export default App;

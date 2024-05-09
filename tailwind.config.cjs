391d09c6bc0170d1252012e4fe847fce12a991e1
yuran1811 <trieuvanbd123@gmail.com>
2024-05-09 18:01:49 +0700

    chore: 🔧 update config file and rename the repo

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,1 +1,4 @@
-module.exports={content:["./index.html","./src/**/*.{vue,js,ts,jsx,tsx}"]}
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+};
